e0c51a9caf4163ce23ce71cbc487de2f11da2df4
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-06-29 17:17:21 +0100

    Filled out posts controller so new and create actions are associated with the relevant logged in user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_152628) do
+ActiveRecord::Schema.define(version: 2021_06_29_160401) do
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_06_29_152628) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
